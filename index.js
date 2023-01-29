bbff66cc8e3c39cec852711f8dc2aaf3d1d46e5c
Maxim Kuzmianok <jst.wrx@gmail.com>
2023-01-30 01:48:36 +0300

    feat: add env file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express')
 const cors = require('cors')
-const http = require("http");
 const { Server } = require("socket.io");
+const express = require('express')
+require('dotenv').config()
+const http = require("http");
 
 const app = express();
 app.use(cors())
@@ -10,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: process.env.PORT,
         methods: ["GET", "POST"],
     },
 });
