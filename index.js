80633cb37f5cf51de79c9e7e0b897e519fd1a5c2
Maxim Kuzmianok <jst.wrx@gmail.com>
2023-01-30 00:42:53 +0300

    feat: connect game logic and sockets

--- index.js
+++ index.js
@@ -1,19 +1,70 @@
 const express = require('express')
 const cors = require('cors')
-require('dotenv').config()
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
-const PORT = process.env.PORT || 8080
-
-app.use(express.json())
 app.use(cors())
 
+const server = http.createServer(app);
 
-const start = async () => {
-    try {
-        app.listen(PORT, () => console.log(`server started, port:${PORT}`))
-    } catch (error) {
-        console.log(error)
-    }
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+
+const users = [];
+
+const addUser = (name, socketId) => {
+    !users.some((user) => user.name === name) &&
+        users.push({ name, socketId })
+    console.log(users)
 }
-start()
+
+let cells = Array(8).fill('')
+
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+
+    socket.on("join_room", (room) => {
+        socket.join(room);
+        console.log(`User with ID: ${socket.id} joined room: ${room}`);
+    });
+
+    socket.on('leave_room', (room) => {
+        socket.leave(room)
+    })
+
+    socket.on('addUser', (name) => {
+        addUser(name, socket.id)
+        io.emit('getUsers', users)
+    });
+
+    socket.on("send_step", (data) => {
+        let room = data.room.toString()
+        cells = data.cells
+        io.sockets.to(room).emit("receive_step", {
+            cells: cells,
+            room: data.room,
+            name: data.name,
+            step: data.step,
+            turn: data.turn
+        });
+    });
+
+    socket.on('restart', (data) => {
+        let room = data.room.toString()
+        cells = data.cells
+        socket.to(room).emit('confirm_restart', data)
+    })
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected", socket.id);
+    });
+});
+
+server.listen(3001, () => {
+    console.log("SERVER IS RUNNING");
+});
